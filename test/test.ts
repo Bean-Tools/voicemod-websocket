63bcebb6f59d3be3d87fb777f466b3a10e5ec4f5
Izzy <360964+izzy@users.noreply.github.com>
2023-06-25 14:04:05 +0200

    Fixed tsc error when no test config was defined

--- test/test.ts
+++ test/test.ts
@@ -2,91 +2,110 @@ import { exit } from 'process';
 import { Soundboard, Voice } from '../src/types';
 import VoicemodWebsocket from '../src/websocket';
 
-import { config } from './config';
+let config;
+import('./config')
+  .then((c) => {
+    if (!c.config) {
+      console.error('Please create a config.ts file in the test directory, see config.ts.example');
+      exit(1);
+    }
 
-const voicemod = new VoicemodWebsocket('127.0.0.1', config.client_key);
+    config = c.config;
 
-// This one logs *everything* that comes from the websocket
-// Usefull to see what's going on e.g. if something isn't working
-// Or something in Voicemod's API documentation was off
-voicemod.on('AllEvents', (...args) => {
-    console.debug('VM.on(AllEvents):::         ', args[0].actionType ? args[0].actionType : args[0]);
-});
+    const voicemod = new VoicemodWebsocket('127.0.0.1', config.client_key);
 
+    // This one logs *everything* that comes from the websocket
+    // Usefull to see what's going on e.g. if something isn't working
+    // Or something in Voicemod's API documentation was off
+    voicemod.on('AllEvents', (...args) => {
+      console.debug(
+        'VM.on(AllEvents):::         ',
+        args[0].actionType ? args[0].actionType : args[0],
+      );
+    });
 
-voicemod.on('ClientRegistrationFailed', () => {
-    console.log('VM.on(ClientRegistrationFailed)');
+    voicemod.on('ClientRegistrationFailed', () => {
+      console.log('VM.on(ClientRegistrationFailed)');
+      exit(1);
+    });
+
+    voicemod.on('UserChanged', (user) => {
+      console.log('VM.on(UserChanged):::       ', user);
+    });
+
+    voicemod.on('UserLicenseChanged', (license) => {
+      console.log('VM.on(UserLicenseChanged):: ', license);
+    });
+
+    voicemod.on('VoiceChanged', (voice: Voice) => {
+      console.log('VM.on(VoiceChanged):::      ', voice.friendlyName || voice.id);
+    });
+
+    voicemod.on('VoiceChangerStatusChanged', (status) => {
+      console.log('VM.on(VoiceChangerStatusChanged)::: ', status);
+    });
+
+    voicemod.on('ClientRegistered', () => {
+      voicemod.getVoices().then((voices) => {
+        console.log(`Found ${voices.length} voices`);
+
+        voicemod.getUserLicense().then((license) => {
+          console.log('License', license);
+        });
+
+        voicemod.getUser().then((user) => {
+          console.log('User', user);
+        });
+
+        voicemod.getActiveSoundboardProfile().then((profile) => {
+          console.log('Active soundboard profile', profile.name);
+        });
+
+        voicemod.getAllSoundboard().then((profiles: Soundboard[]) => {
+          console.log('Number of Soundboard profiles', profiles.length);
+        });
+
+        voicemod.getBackgroundEffectsStatus().then((status) => {
+          console.log('Background effects status', status);
+        });
+
+        voicemod.getVoiceChangerStatus().then((status) => {
+          console.log('Voice changer status', status);
+        });
+
+        voicemod.getCurrentVoice().then((voice) => {
+          console.log('Current voice', voice.friendlyName);
+
+          const random_voice = voices[Math.floor(Math.random() * voices.length)];
+          console.log('Setting voice to', random_voice.friendlyName);
+          voicemod.setVoice(random_voice.id);
+        });
+
+        voicemod.getMuteMicStatus().then((mute) => {
+          console.log('Mute status', mute);
+        });
+
+        voicemod.getCurrentVoice().then((voice) => {
+          console.log('Current voice', voice.friendlyName);
+
+          // Comment this out if you want to test listening to events instead of
+          // closing the connection at the end of the script
+          //voicemod.disconnect();
+        });
+      });
+    });
+
+    voicemod
+      .connect()
+      .then(() => {
+        console.log('Connected');
+      })
+      .catch((err) => {
+        console.error('Error connecting', err);
+      });
+  })
+  .catch((err) => {
+    console.error('Could not load config.ts');
+    console.error(err);
     exit(1);
-});
-
-voicemod.on('UserChanged', (user) => {
-    console.log('VM.on(UserChanged):::       ', user);
-});
-
-voicemod.on('UserLicenseChanged', (license) => {
-    console.log('VM.on(UserLicenseChanged):: ', license);
-});
-
-voicemod.on('VoiceChanged', (voice: Voice) => {
-    console.log('VM.on(VoiceChanged):::      ', voice.friendlyName || voice.id);
-});
-
-voicemod.on('VoiceChangerStatusChanged', (status) => {
-    console.log('VM.on(VoiceChangerStatusChanged)::: ', status);
-});
-
-voicemod.on('ClientRegistered', () => {
-  voicemod.getVoices().then((voices) => {
-    console.log(`Found ${voices.length} voices`);
-
-    voicemod.getUserLicense().then((license) => {
-      console.log('License', license);
-    });
-
-    voicemod.getUser().then((user) => {
-      console.log('User', user);
-    });
-
-    voicemod.getActiveSoundboardProfile().then((profile) => {
-      console.log('Active soundboard profile', profile.name);
-    });
-
-    voicemod.getAllSoundboard().then((profiles: Soundboard[]) => {
-      console.log('Number of Soundboard profiles', profiles.length);
-    });
-
-    voicemod.getBackgroundEffectsStatus().then((status) => {
-      console.log('Background effects status', status);
-    });
-
-    voicemod.getVoiceChangerStatus().then((status) => {
-      console.log('Voice changer status', status);
-    });
-
-    voicemod.getCurrentVoice().then((voice) => {
-      console.log('Current voice', voice.friendlyName);
-
-      const random_voice = voices[Math.floor(Math.random() * voices.length)];
-      console.log('Setting voice to', random_voice.friendlyName);
-      voicemod.setVoice(random_voice.id);
-    });
-
-    voicemod.getMuteMicStatus().then((mute) => {
-      console.log('Mute status', mute);
-    });
-
-    voicemod.getCurrentVoice().then((voice) => {
-      console.log('Current voice', voice.friendlyName);
-
-      // Comment this out if you want to test listening to events instead of
-      // closing the connection at the end of the script
-      //voicemod.disconnect();
-    });
   });
-});
-
-voicemod.connect().then(() => {
-  console.log('Connected');
-}).catch((err) => {
-  console.error('Error connecting', err);
-});
